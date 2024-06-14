d8fd56cb9063017ccd2c24e46f9583d8b09fffa5
Faiyaaz Ismail <125645430+f4iyaaz@users.noreply.github.com>
2024-06-14 21:00:33 +0600

    Update 4-first_and_last_position.cpp

--- 3-Binary search/4-first_and_last_position.cpp
+++ 3-Binary search/4-first_and_last_position.cpp
@@ -7,66 +7,36 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         
+         vector<int> result = {-1, -1};
         int low = 0;
         int high = nums.size() - 1;
-        int mid = 0;
-        int index = -1;
-        vector<int> v;
 
-        if(nums.size() == 0) {
-            return v = {-1, -1};
-        }
+        if (nums.empty())
+            return result;
 
-        while(low <= high) {
-            mid = (low + high) / 2;
-            if(target == nums[mid]) {
-                index = mid;
-                break;
-            }
-            else if(target < nums[mid]) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                result[1] = mid;
+
+                // Expand the range to the left
+                while (result[0] > 0 && nums[result[0] - 1] == target)
+                    result[0]--;
+
+                // Expand the range to the right
+                while (result[1] < nums.size() - 1 && nums[result[1] + 1] == target)
+                    result[1]++;
+
+                return result;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
                 high = mid - 1;
             }
-            else if(target > nums[mid]) {
-                low = mid + 1;
-            }
         }
 
-
-        if(index == 0) {
-            if(nums[index] == nums[index + 1]) {
-                v = {0, 1};
-                return v;
-            }
-            else if(nums[index] != nums[index + 1]) {
-                v = {index, index};
-                return v;
-            }
-        }
-        else if(index == nums.size() - 1) {
-            if(nums[index - 1] == nums[index]) {
-                v = {index - 1, index};
-                return v;
-            }
-            else if(nums[index - 1] != nums[index]) {
-                v = {index, index};
-                return v;
-            }
-        }
-        else {
-            if(nums[index - 1] == nums[index]) {
-                v = {index - 1, index};
-                return v;
-            }
-            else if(nums[index + 1] == nums[index]) {
-                v = {index, index + 1};
-                return v;
-            }
-            else {
-                v = {index, index};
-                return v;
-            }
-        }
-        return v = {-1, -1};
+        return result;
     }
 };
 
