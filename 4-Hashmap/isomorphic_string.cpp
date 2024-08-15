ed3aa6dd7999889d00b6beefc8f5c7b67bcada2b
Faiyaaz Ismail <125645430+f4iyaaz@users.noreply.github.com>
2024-08-15 13:37:51 +0600

    Update isomorphic_string.cpp

--- 4-Hashmap/isomorphic_string.cpp
+++ 4-Hashmap/isomorphic_string.cpp
@@ -5,27 +5,45 @@ using namespace std;
 
 class Solution {
 public:
+    // bool check(unordered_map<char, char> &st, unordered_map<char, char> ts) {
+    //     return true;
+    // }
+    // s can't map more than one characters of t
+    // t can't map more than one characters of s
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, int> sCount;    
-        unordered_map<char, int> tCount;    
-        int length = s.size(); // size of 's' is equal to 't'
-        // initialize
-        for(int i = 0; i < length; i++) {
-            sCount[s[i]] = 0;
+        unordered_map<char, char> s_to_t;
+        unordered_map<char, char> t_to_s;
+        for(int i = 0; i < s.size(); i++) {
+            if(s_to_t.empty()) {
+                s_to_t[s[i]] = t[i];
+                t_to_s[t[i]] = s[i];
+            }
+            else {
+                if(s_to_t.count(s[i]) || t_to_s.count(t[i])) {
+                    if(s_to_t[s[i]] != t[i]) {
+                        return false;
+                    }
+                    if(t_to_s[t[i]] != s[i]) {
+                        return false;
+                    }
+                }
+                else {
+                    s_to_t[s[i]] = t[i];
+                    t_to_s[t[i]] = s[i];
+                }
+            }
         }
-        for(int i = 0; i < length; i++) {
-            tCount[t[i]] = 0;
-        }
-        // counting
-        for(int i = 0; i < length; i++) {
-            
-        }
+        return true;
     }
 };
 
 int main() {
     Solution sol;
-    string s = "egg";
-    string t = "add";
+    string s = "bbbaaaba";
+    string t = "aaabbbba";
+    // string s = "egg";
+    // string t = "add";
+    // string s = "badc";
+    // string t = "baba";
     cout << sol.isIsomorphic(s, t);
 }
