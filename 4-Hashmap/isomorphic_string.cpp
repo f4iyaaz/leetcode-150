bc26aa57ccdb6b3d85caede6153cd4a6cc78f42f
Faiyaaz Ismail <125645430+f4iyaaz@users.noreply.github.com>
2024-08-15 09:50:38 +0600

    Add files via upload

---
+++ 4-Hashmap/isomorphic_string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        return true;    
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "aa";
+    string t = "bb";
+    cout << sol.isIsomorphic(s, t);
+}
