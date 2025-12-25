064598e92377789a2f3fc6a918668978ca4deb92
Faiyaaz Ismail <125645430+f4iyaaz@users.noreply.github.com>
2025-12-25 23:17:01 +0600

    Add removeDuplicates function for sorted linked list
    Implement function to remove duplicates from a sorted linked list.

---
+++ 6-Linked_List/remove_duplicate_from_sorted_list.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
+        if head.next is None:
+            return head
+        left = head
+        right = head.next
+        while right.next is not None:
+            if left.val != right.val:
+                left.next = right
+                left = right
+                right = right.next
+            else:
+                right = right.next
+
+        if left.val != right.val:
+            left.next = right
+        else:
+            left.next = None
+
+        return head
