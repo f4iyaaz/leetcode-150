c3b1385aaa7cf670ccc73a21168aabc3d00e4c6c
Faiyaaz Ismail <125645430+f4iyaaz@users.noreply.github.com>
2025-12-28 20:33:57 +0600

    Add middleNode function for linked list
    Implement function to find the middle of a linked list.

---
+++ 6-Linked_List/middle_of_linked_list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next is None:
+            return head
+        hare = head
+        tortoise = head
+        while hare and hare.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            
+        return tortoise
+
+# Beats 100% of runtimes
+# Beats 83.75% of memories
