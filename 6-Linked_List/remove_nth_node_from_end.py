85e7d07d88c894482461726fe5beb6e0efb65327
f4iyaaz <125645430+f4iyaaz@users.noreply.github.com>
2025-12-28 22:54:31 +0600

    Add removeNthFromEnd function for linked list
    Implement function to remove the nth node from the end of a linked list.

---
+++ 6-Linked_List/remove_nth_node_from_end.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if head.next is None:
+            return None
+        dummy = ListNode(0)
+        dummy.next = head
+        ahead = dummy
+        behind = dummy
+        flag = False
+        for _ in range(n + 1):
+            ahead = ahead.next
+        while ahead:
+            flag = True
+            behind = behind.next
+            ahead = ahead.next
+        if flag:
+            behind.next = behind.next.next
+            return head
+        else:
+            dummy.next = dummy.next.next
+            head = dummy.next
+            return head
+
+
+# Runtime: beats 100%
+# Memory: beats 88.42%
