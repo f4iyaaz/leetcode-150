64d239728c39d16535c913111c5b745af040a59d
f4iyaaz <125645430+f4iyaaz@users.noreply.github.com>
2026-01-20 21:31:45 +0600

    Refactor longest substring function for efficiency
    Refactor the longest substring without repeating characters logic to use a counter and two pointers for better efficiency.

--- 9. Sliding Window/longest_substring_without_rep.py
+++ 9. Sliding Window/longest_substring_without_rep.py
@@ -4,21 +4,28 @@ class Solution:
             return 0
         if len(s) == 1:
             return 1
-        length = 0
-        tracker = set()
-        l, r = 0, 0
-        while r < len(s):
-            if s[r] not in tracker:
-                tracker.add(s[r])
-                r += 1
-            else:
-                length = max((r - l), length)
-                if s[l] != s[r]:
-                    l += 1
+        counter = {}
+        left, right = 0, 0
+        size = 0
+        length = len(s)
+        for c in s:
+            counter[c] = 0
+        while right < length:
+            if counter[s[right]] < 2:
+                counter[s[right]] += 1
+                if counter[s[right]] > 1:
+                    continue
                 else:
-                    l += 1
-                    tracker.clear()
-                    for i in range(l, r):
-                        tracker.add(s[i])
-        length = max((r - l), length)
-        return length
+                    right += 1
+            else:
+                if s[left] == s[right]:
+                    size = max(size, (right - left))
+                    counter[s[left]] -= 1
+                    left += 1
+                    right += 1
+                else:
+                    size = max(size, (right - left))
+                    counter[s[left]] -= 1
+                    left += 1
+        size = max(size, (right - left))
+        return size
