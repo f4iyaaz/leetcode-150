a64c6f2f045919bdb170cebd362681091335fb83
f4iyaaz <125645430+f4iyaaz@users.noreply.github.com>
2026-01-23 20:55:49 +0600

    Create fibonacci.py

---
+++ 10. Dynamic Programming/fibonacci.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {}
+        def fibonacci(n):
+            if n in memo:
+                return memo[n]
+            if n == 1:
+                return 1
+            elif n == 0:
+                return 0
+            memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            return memo[n]
+        result = fibonacci(n)
+        return result
