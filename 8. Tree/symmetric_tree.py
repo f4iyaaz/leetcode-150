0cc1bb9283da6e08fd836d0df13a094206b8e8c8
f4iyaaz <125645430+f4iyaaz@users.noreply.github.com>
2026-02-02 22:32:45 +0600

    Add symmetric tree check function
    Implement a function to check if a binary tree is symmetric.

---
+++ 8. Tree/symmetric_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def symmetric(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            if root1.val != root2.val:
+                return False
+            return symmetric(root1.left, root2.right) and symmetric(root2.left, root1.right)
+
+        return symmetric(root.left, root.right)
+
+  # Runtime: Beats 100%
