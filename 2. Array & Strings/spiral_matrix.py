763c9552f1294016c15b9e9b41ff151b0d718bcb
f4iyaaz <faiyaazismail@gmail.com>
2026-08-04 22:41:21 +0600

    solution coming soon

--- 2. Array & Strings/spiral_matrix.py
+++ 2. Array & Strings/spiral_matrix.py
@@ -1,15 +1,7 @@
 class Solution:
     def spiralOrder(self, matrix):
-        row_length = len(matrix[0])
-        row = len(matrix)
-        dummy_result = []
-
-        # I think a while loop would be more suitable here instead of for loop
-        for i in range(row_length):
-            for num in matrix[i]:
-                dummy_result.append(num)
-
-        return dummy_result
+        # figured it out
+        pass
 
 sol = Solution()
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
