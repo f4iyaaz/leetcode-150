45b7a6af3922c433f041432dd9c2323f519fc4e9
f4iyaaz <faiyaazismail@gmail.com>
2026-08-04 23:37:04 +0600

    solution coming soon

--- 2. Array & Strings/spiral_matrix.py
+++ 2. Array & Strings/spiral_matrix.py
@@ -1,7 +1,19 @@
 class Solution:
     def spiralOrder(self, matrix):
-        # figured it out
-        pass
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, 0
+        UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3
+
+        UP_WALL = 0
+        RIGHT_WALL = n
+        DOWN_WALL = m
+        LEFT_WALL = -1
+
+        result = []
+        direction = RIGHT
+
+        while result != (m * n):
+            pass
 
 sol = Solution()
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
